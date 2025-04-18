280391257af243458ae0b97adde9cf2973b6c2fa
hhhhea <haechang977@gmail.com>
2025-04-18 19:19:04 +0900

    kkjj-17 <feature>(<Hotel>) : 호텔서비스 구상
    패키지에 포함시키기 위한 호텔 엔티티 구상

---
+++ src/main/java/renewal/awesome_travel_backoffice/hotel/utiles/HotelType.java
@@ -0,0 +1,15 @@
+package renewal.awesome_travel_backoffice.hotel.utiles;
+
+public enum HotelType {
+    HOTEL,
+    MOTEL,
+    RESORT,
+    APARTMENT,
+    GUESTHOUSE,
+    PENSION,
+    HOSTEL,
+    HOMESTAY,
+    LODGE,
+    VILLA,
+    ETC
+}
