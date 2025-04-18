ac8ef340db88d55b9b94242a60fc3bafb3108a53
JinK001 <kyj9447@gmail.com>
2025-04-18 00:56:30 +0000

    kkjj-13 <feature>(<Tour>) : 컨트롤러에 레이아웃 사용하도록 수정

--- src/main/java/renewal/awesome_travel_backoffice/controller/MainController.java
+++ src/main/java/renewal/awesome_travel_backoffice/controller/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.core.Authentication;
+
 import org.springframework.ui.Model;
 
 import lombok.RequiredArgsConstructor;
@@ -18,9 +20,14 @@ public class MainController {
     public String loginPage(@RequestParam(value = "error", required = false) String error) {
         return "login";
     }
+    
     @GetMapping
-    public String main(Model model) {
-        return "main";
+    public String main(Model model, Authentication authentication) {
+        
+        model.addAttribute("title", "Home");
+        model.addAttribute("content", "components/main");
+        
+        return "layout";
     }
     
 }
--- src/main/java/renewal/awesome_travel_backoffice/controller/TourController.java
+++ src/main/java/renewal/awesome_travel_backoffice/controller/TourController.java
@@ -1,5 +1,6 @@
 package renewal.awesome_travel_backoffice.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,36 +26,60 @@ public class TourController {
     // 투어 목록
     @GetMapping
     public String productIndex(Model model) {
+
         List<Tour> list = tourRepo.findAll();
         model.addAttribute("tourList", list);
-        return "tour";
+        model.addAttribute("title", "Tour List");
+        model.addAttribute("content", "components/tour");
+
+        return "layout";
     }
 
     // 새 투어
     @GetMapping("/new")
     public String newTravel(Model model) {
+
         Tour blank = new Tour();
         model.addAttribute("tour", blank);
-        return "tourDetail";
+        model.addAttribute("title", "New Tour");
+        model.addAttribute("content", "components/tourDetail");
+
+        return "layout";
     }
     // 새 투어 등록
     @PostMapping("/new")
     public String submitTravel(@ModelAttribute Tour tour) {
+
         tourRepo.save(tour);
+
         return "redirect:/tour";
     }
     
     // 특정 투어
     @GetMapping("/{id}")
     public String selectTravel(@PathVariable Long id, Model model) {
+
         Tour tour = tourRepo.getReferenceById(id);
         model.addAttribute("tour", tour);
-        return "tourDetail";
+        model.addAttribute("title", "Tour "+tour.getName());
+        model.addAttribute("content", "components/tourDetail");
+
+        return "layout";
     }
     // 특정 투어 수정
     @PostMapping("/{id}")
     public String submitSelectedTravel(@ModelAttribute Tour tour) {
+
         tourRepo.save(tour);
+
+        return "redirect:/tour";
+    }   
+    // 특정 투어 삭제
+    @DeleteMapping("/{id}")
+    public String deleteSelectedTravel(@ModelAttribute Tour tour) {
+
+        tourRepo.delete(tour);
+
         return "redirect:/tour";
     }
 }
