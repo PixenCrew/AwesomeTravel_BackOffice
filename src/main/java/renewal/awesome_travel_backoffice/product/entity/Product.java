f1dd3fa41016233e2ccaaa0302a0f2e6dfe1e6c9
JinK001 <kyj9447@gmail.com>
2025-05-16 13:29:47 +0000

    minor changes

--- src/main/java/renewal/awesome_travel_backoffice/product/entity/Product.java
+++ src/main/java/renewal/awesome_travel_backoffice/product/entity/Product.java
@@ -1,5 +1,6 @@
 package renewal.awesome_travel_backoffice.product.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,7 +17,8 @@ public class Product {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name = "product_id")
+    private Long id;
     private String title;
     private Integer price;
 
