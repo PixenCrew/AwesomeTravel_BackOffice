b9baacfe88b87b2106d21d9468e0a8d117728464
JinK001 <kyj9447@gmail.com>
2025-05-16 10:52:46 +0000

    kkjj-13 <feature>(<Images>) 이미지 URL로 GET 가능

--- src/main/java/renewal/awesome_travel_backoffice/config/WebConfig.java
+++ src/main/java/renewal/awesome_travel_backoffice/config/WebConfig.java
@@ -1,25 +1,21 @@
 package renewal.awesome_travel_backoffice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${imageLocation}")
     private String uploadDir;
 
-    // @Override
-    // public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
-    // registry.addResourceHandler("/images/**")
-    // .addResourceLocations("file:" + new File(uploadDir).getAbsolutePath() + "/");
-    // }
-
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
-        String currentWorkingDir = System.getProperty("user.dir");
+        String fullPath = "file:" + uploadDir + "/";
+        // System.out.println("fullPath : "+fullPath);
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:" + currentWorkingDir + "/images/");
+                .addResourceLocations(fullPath);
     }
 }
