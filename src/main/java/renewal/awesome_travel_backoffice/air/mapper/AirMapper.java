5a234debe66a9dfb77020cd317681fed014b593e
JinK001 <kyj9447@gmail.com>
2025-07-22 17:27:32 +0900

    kkjj-28 <feature> (<Air>) : 미사용 컴포넌트 삭제

--- src/main/java/renewal/awesome_travel_backoffice/air/mapper/AirMapper.java
+++
@@ -1,8 +0,0 @@
-package renewal.awesome_travel_backoffice.air.mapper;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class AirMapper {
-
-}
--- src/main/java/renewal/awesome_travel_backoffice/air/utiles/TripType.java
+++
@@ -1,7 +0,0 @@
-package renewal.awesome_travel_backoffice.air.utiles;
-
-public enum TripType {
-    ONE_WAY,
-    ROUND_TRIP,
-    MULTI
-}
