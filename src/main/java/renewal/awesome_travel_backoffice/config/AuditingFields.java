8c2e99032b8fe4f26f833735b53e0443e2151418
JinK001 <kyj9447@gmail.com>
2025-07-25 21:32:21 +0900

    minor changes

--- src/main/java/renewal/awesome_travel_backoffice/config/AuditingFields.java
+++ src/main/java/renewal/awesome_travel_backoffice/config/AuditingFields.java
@@ -11,8 +11,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDateTime;
 
 @Getter
@@ -21,7 +19,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public class AuditingFields {
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt; //생성일시
@@ -30,7 +27,6 @@ public class AuditingFields {
     @Column(nullable = false, updatable = false, length = 100)
     private String createdBy; //생성자
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = true)
     private LocalDateTime modifiedAt; //수정일시
