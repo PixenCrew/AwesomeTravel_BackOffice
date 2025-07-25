448c64f2eed8da91aa65b073f40a0100a2a7d4c4
JinK001 <kyj9447@gmail.com>
2025-07-25 21:26:02 +0900

    kkjj-28 <feature> (<Air>) : 필터 Specification SeatClasses 기준으로 수정

--- src/main/java/renewal/awesome_travel_backoffice/air/repository/AirSpecification.java
+++ src/main/java/renewal/awesome_travel_backoffice/air/repository/AirSpecification.java
@@ -14,50 +14,55 @@ import renewal.awesome_travel_backoffice.air.entity.Airline;
 import renewal.awesome_travel_backoffice.air.entity.SeatClass;
 import renewal.awesome_travel_backoffice.air.utiles.AirStatus;
 import renewal.awesome_travel_backoffice.air.utiles.FlightType;
+import renewal.awesome_travel_backoffice.air.utiles.SeatClassType;
 
 public class AirSpecification {
 
     // code LIKE %code%
-    public static Specification<Air> codeContains(String code) {
+    public static Specification<SeatClass> codeContains(String code) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (!StringUtils.hasText(code))
                 return null;
             return builder.like(
-                    builder.lower(root.get("code")),
+                    builder.lower(seatJoin.get("code")),
                     "%" + code.toLowerCase() + "%");
         };
     }
 
     // airline IN (list)
-    public static Specification<Air> airlinesIn(List<String> codes) {
+    public static Specification<SeatClass> airlinesIn(List<String> codes) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (codes == null || codes.isEmpty()) {
                 return null;
             }
-            Join<Air, Airline> join = root.join("airline", JoinType.INNER);
+            Join<Air, Airline> join = seatJoin.join("airline", JoinType.INNER);
             return join.get("code").in(codes);
         };
     }
 
     // InfantSeatsRequired == value
-    public static Specification<Air> getInfantSeatsRequired(Boolean check) {
+    public static Specification<SeatClass> getInfantSeatsRequired(Boolean check) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (check == null)
                 return null;
-            Join<Air, Airline> join = root.join("airline", JoinType.INNER);
+            Join<Air, Airline> join = seatJoin.join("airline", JoinType.INNER);
             return builder.equal(join.get("infantSeatsRequired"), check);
         };
     }
 
     // departDate BETWEEN from AND to
-    public static Specification<Air> departDateBetween(LocalDate from, LocalDate to) {
+    public static Specification<SeatClass> departDateBetween(LocalDate from, LocalDate to) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (from != null && to != null) {
-                return builder.between(root.get("departDate"), from, to);
+                return builder.between(seatJoin.get("depart_time"), from, to);
             } else if (from != null) {
-                return builder.greaterThanOrEqualTo(root.get("departDate"), from);
+                return builder.greaterThanOrEqualTo(seatJoin.get("depart_time"), from);
             } else if (to != null) {
-                return builder.lessThanOrEqualTo(root.get("departDate"), to);
+                return builder.lessThanOrEqualTo(seatJoin.get("depart_time"), to);
             } else {
                 return null;
             }
@@ -65,14 +70,15 @@ public class AirSpecification {
     }
 
     // arriveDate BETWEEN from AND to
-    public static Specification<Air> arriveDateBetween(LocalDate from, LocalDate to) {
+    public static Specification<SeatClass> arriveDateBetween(LocalDate from, LocalDate to) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (from != null && to != null) {
-                return builder.between(root.get("arriveDate"), from, to);
+                return builder.between(seatJoin.get("arrive_time"), from, to);
             } else if (from != null) {
-                return builder.greaterThanOrEqualTo(root.get("arriveDate"), from);
+                return builder.greaterThanOrEqualTo(seatJoin.get("arrive_time"), from);
             } else if (to != null) {
-                return builder.lessThanOrEqualTo(root.get("arriveDate"), to);
+                return builder.lessThanOrEqualTo(seatJoin.get("arrive_time"), to);
             } else {
                 return null;
             }
@@ -80,122 +86,109 @@ public class AirSpecification {
     }
 
     // depart == value
-    public static Specification<Air> departEquals(String depart) {
+    public static Specification<SeatClass> departEquals(String depart) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (!StringUtils.hasText(depart))
                 return null;
-            return builder.equal(root.get("depart"), depart);
+            return builder.equal(seatJoin.get("departAirport"), depart);
         };
     }
 
     // arrive == value
-    public static Specification<Air> arriveEquals(String arrive) {
+    public static Specification<SeatClass> arriveEquals(String arrive) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (!StringUtils.hasText(arrive))
                 return null;
-            return builder.equal(root.get("arrive"), arrive);
+            return builder.equal(seatJoin.get("arrive"), arrive);
         };
     }
 
     // stopovers == value
-    public static Specification<Air> stopoversEquals(Long stopovers) {
+    public static Specification<SeatClass> stopoversEquals(Long stopovers) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT);
             if (stopovers == null)
                 return null;
-            return builder.equal(root.get("stopovers"), stopovers);
+            return builder.equal(seatJoin.get("stopovers"), stopovers);
         };
     }
 
     // stopovers BETWEEN from AND to
-    public static Specification<Air> stopoversBetween(Long minStopovers, Long maxStopovers) {
+    public static Specification<SeatClass> stopoversBetween(Long minStopovers, Long maxStopovers) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT); 
             if (minStopovers != null && maxStopovers != null) {
-                return builder.between(root.get("stopovers"), minStopovers, maxStopovers);
+                return builder.between(seatJoin.get("stopovers"), minStopovers, maxStopovers);
             } else if (minStopovers != null) {
-                return builder.greaterThanOrEqualTo(root.get("stopovers"), minStopovers);
+                return builder.greaterThanOrEqualTo(seatJoin.get("stopovers"), minStopovers);
             } else if (maxStopovers != null) {
-                return builder.lessThanOrEqualTo(root.get("stopovers"), maxStopovers);
+                return builder.lessThanOrEqualTo(seatJoin.get("stopovers"), maxStopovers);
             } else {
                 return null;
             }
         };
     }
-    // // seatCount BETWEEN min AND max
-    // public static Specification<Air> seatCountBetween(Long min, Long max) {
-    // return (root, query, builder) -> {
-    // if (min != null && max != null) {
-    // return builder.between(root.get("seatCount"), min, max);
-    // } else if (min != null) {
-    // return builder.greaterThanOrEqualTo(root.get("seatCount"), min);
-    // } else if (max != null) {
-    // return builder.lessThanOrEqualTo(root.get("seatCount"), max);
-    // } else {
-    // return null;
-    // }
-    // };
-    // }
 
     // flightType == value
-    public static Specification<Air> flightTypeEquals(FlightType type) {
+    public static Specification<SeatClass> flightTypeEquals(FlightType type) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT); 
             if (type == null)
                 return null;
-            return builder.equal(root.get("flightType"), type);
+            return builder.equal(seatJoin.get("flightType"), type);
         };
     }
 
     // status == value
-    public static Specification<Air> statusEquals(AirStatus status) {
+    public static Specification<SeatClass> statusEquals(AirStatus status) {
         return (root, query, builder) -> {
+            Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT); 
             if (status == null)
                 return null;
-            return builder.equal(root.get("status"), status);
+            return builder.equal(seatJoin.get("status"), status);
         };
     }
 
     // seatClasses.price BETWEEN min AND max (JOIN & DISTINCT)
-    public static Specification<Air> priceBetween(BigDecimal min, BigDecimal max) {
+    public static Specification<SeatClass> priceBetween(BigDecimal min, BigDecimal max) {
         return (root, query, builder) -> {
             if (min == null && max == null || query == null)
                 return null;
-
-            Join<Air, SeatClass> join = root.join("seatClasses", JoinType.LEFT);
+            // Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT); 
             if (min != null && max != null) {
                 query.distinct(true);
-                return builder.between(join.get("price"), min, max);
+                return builder.between(root.get("price"), min, max);
             } else if (min != null) {
                 query.distinct(true);
-                return builder.greaterThanOrEqualTo(join.get("price"), min);
+                return builder.greaterThanOrEqualTo(root.get("price"), min);
             } else {
                 query.distinct(true);
-                return builder.lessThanOrEqualTo(join.get("price"), max);
+                return builder.lessThanOrEqualTo(root.get("price"), max);
             }
         };
     }
 
     // availableSeats >= value
-    public static Specification<Air> availableSeatsMore(Long more) {
+    public static Specification<SeatClass> availableSeatsMore(Long more) {
         return (root, query, builder) -> {
             if (more == null || query == null)
                 return null;
-            Join<Air, SeatClass> join = root.join("seatClasses", JoinType.LEFT);
-            query.distinct(true);
-            return builder.greaterThanOrEqualTo(join.get("availableSeats"), more);
+            // Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT); 
+            // query.distinct(true);
+            return builder.greaterThanOrEqualTo(root.get("availableSeats"), more);
         };
     }
 
-    // // DTO 전체를 한 번에 묶는 메서드
-    // public static Specification<Air> byFilter(AirFilterDTO f) {
-    // return Specification.where(codeContains(f.getCode()))
-    // .and(airlinesIn(f.getArilines()))
-    // .and(departDateBetween(f.getDepartDateFrom(), f.getDepartDateTo()))
-    // .and(arriveDateBetween(f.getArriveDateFrom(), f.getArriveDateTo()))
-    // .and(departEquals(f.getDepart()))
-    // .and(arriveEquals(f.getArrive()))
-    // .and(stopoversEquals(f.getStopovers()))
-    // .and(seatCountBetween(f.getStartCount(), f.getEndCount()))
-    // .and(flightTypeEquals(f.getFlightType()))
-    // .and(statusEquals(f.getStatus()))
-    // .and(priceBetween(f.getMinPrice(), f.getMaxPrice()));
-    // }
+    // SeatClassType == value
+    public static Specification<SeatClass> seatClassEquals(SeatClassType type) {
+        return (root, query, builder) -> {
+            query.distinct(true);
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+type);
+            // SeatClass 조인
+            // Join<SeatClass, Air> seatJoin = root.join("air", JoinType.LEFT); 
+            return builder.equal(root.get("classType"), type);
+        };
+    }  
 }
--- src/main/java/renewal/awesome_travel_backoffice/air/repository/SeatClassRepository.java
+++ src/main/java/renewal/awesome_travel_backoffice/air/repository/SeatClassRepository.java
@@ -1,10 +1,14 @@
 package renewal.awesome_travel_backoffice.air.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import renewal.awesome_travel_backoffice.air.entity.SeatClass;
 
-import java.util.List;
+public interface SeatClassRepository extends JpaRepository<SeatClass, Long>, JpaSpecificationExecutor<SeatClass>   {
+    Page<SeatClass> findAll(Specification<SeatClass> spec, Pageable pageable);
 
-public interface SeatClassRepository extends JpaRepository<SeatClass, Long> {
-    List<SeatClass> findByAir_Id(Long airId);
 }
