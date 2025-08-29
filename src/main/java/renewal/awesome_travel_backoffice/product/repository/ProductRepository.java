e82789e70b7027df4783a66a1c679d7e48d47402
JinK001 <kyj9447@gmail.com>
2025-08-29 13:24:37 +0900

    kkjj-28 <feature> (<Tour>) Tour삭제시 항공, 호텔 취소로직 분리

--- src/main/java/renewal/awesome_travel_backoffice/product/repository/ProductRepository.java
+++ src/main/java/renewal/awesome_travel_backoffice/product/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package renewal.awesome_travel_backoffice.product.repository;
 
 import renewal.awesome_travel_backoffice.product.entity.Product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product>{
     
+    List<Product> findByTourId(Long id);
+
 }
