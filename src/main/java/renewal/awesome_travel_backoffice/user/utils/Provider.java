4075affd5f30aa561c5971e7f448116cff2c796a
JinK001 <kyj9447@gmail.com>
2025-09-01 15:34:59 +0900

    kkjj-32 <refactor> Enum 해당 클래스 내부로 통합

--- src/main/java/renewal/awesome_travel_backoffice/user/utils/Provider.java
+++
@@ -1,5 +0,0 @@
-package renewal.awesome_travel_backoffice.user.utils;
-
-public enum Provider {
-    LOCAL, GOOGLE, NAVER
-}
--- src/main/java/renewal/awesome_travel_backoffice/user/utils/Status.java
+++
@@ -1,5 +0,0 @@
-package renewal.awesome_travel_backoffice.user.utils;
-
-public enum Status {
-    ACTIVE, WITHDRAWN, BANNED
-}
