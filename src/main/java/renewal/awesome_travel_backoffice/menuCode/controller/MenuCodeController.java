b915be3f1c9361324746e989d3634cdc3bdf2af4
JinK001 <kyj9447@gmail.com>
2025-10-16 03:10:13 +0000

    kkjj-38 <feature> (<MenuCode>) MenuCode CRUD

--- src/main/java/renewal/awesome_travel_backoffice/menuCode/controller/MenuCodeController.java
+++ src/main/java/renewal/awesome_travel_backoffice/menuCode/controller/MenuCodeController.java
@@ -5,18 +5,16 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.RequiredArgsConstructor;
-import renewal.common.repository.MenuCodeRepository;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
 import renewal.common.entity.MenuCode;
+import renewal.common.repository.MenuCodeRepository;
 
 @RequiredArgsConstructor
 @RequestMapping("/menu-code")
@@ -47,10 +45,21 @@ public class MenuCodeController {
         return "layout";
     }
 
+    @GetMapping("/new")
+    public String newMenuCode(Model model) {
+
+        MenuCode menuCode = new MenuCode();
+
+        model.addAttribute("title", "New MenuCode");
+        model.addAttribute("menuCode", menuCode);
+        model.addAttribute("content", "components/menuCodeDetail");
+
+        return "layout";
+    }
     
     @PostMapping("/new")
-    public String createMenuCodeDetail(@ModelAttribute MenuCode menuCode, Model model) {
-        
+    public String submitNewMenuCode(@ModelAttribute MenuCode menuCode) {
+        System.out.println(menuCode.getDetails());
         menuCodeRepository.save(menuCode);
 
         return "redirect:/menu-code";
